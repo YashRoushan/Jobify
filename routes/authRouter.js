632a0e49fb5a9f1093a78c907edd730bc87b1b78
YashRoushan <yashroushanprogramming@gmail.com>
2023-11-18 03:24:09 -0400

    validate login

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 const router = Router();
 import { login, register } from '../controllers/authController.js';
-import { validRegisterInput } from "../middleware/validationMiddleware.js";
+import { validateRegisterInput, validateLoginInput } from "../middleware/validationMiddleware.js";
 
 
-router.post('/register', validRegisterInput, register);
-router.post('/login', login);
+router.post('/register', validateRegisterInput, register);
+router.post('/login', validateLoginInput, login);
 
 export default router;
