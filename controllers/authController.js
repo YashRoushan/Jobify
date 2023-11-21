340a918e63c5f299f7700cbc2afdba6813647967
yash roushan <yashroushan@gmail.com>
2023-11-21 15:58:32 -0400

    added login login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
-import { hashPassword } from '../utils/passwordUtils.js';
+import { comparePassword, hashPassword } from '../utils/passwordUtils.js';
+import { UnauthenticatedError } from '../errors/customErrors.js';
 
 
 export const register = async (req, res) => {
@@ -14,5 +15,14 @@ export const register = async (req, res) => {
 }
 
 export const login = async (req, res) => {
+
+    // check if user exists 
+    // check if password is correct
+
+    const user = await User.findOne({ email: req.body.email });
+
+    const isValidUser = user && await comparePassword(req.body.password, user.password);
+
+    if (!isValidUser) throw new UnauthenticatedError('invalid credentials');
     res.send('login');
 }
