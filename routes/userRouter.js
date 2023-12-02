3971214b819061972cd77e76e6221fca1db968ce
yash roushan <yashroushan@gmail.com>
2023-12-01 21:49:18 -0400

    User Routes

---
+++ routes/userRouter.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { getApplicationStats, getCurrentUser, updateUser } from "../controllers/userController.js";
+import { updateJob } from "../controllers/jobController.js";
+const router = Router();
+
+
+router.get('/current-user', getCurrentUser);
+router.get('/admin/app-stats', getApplicationStats);
+router.patch('/update-user', updateUser);
+
+export default router;
