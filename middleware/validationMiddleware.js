2fa128dfe341ad3d35c337de933b6c4363d5e1e5
yash roushan <yashroushan@gmail.com>
2023-12-11 14:44:40 -0400

    update user

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -68,3 +68,16 @@ export const validateLoginInput = withValidateErrors([
     body('email').notEmpty().withMessage('email is required').isEmail().withMessage('invalid email format'),
     body('password').notEmpty().withMessage('password is required'),
 ]);
+
+// check
+export const validateUpdateUserInput = withValidateErrors([
+    body('name').notEmpty().withMessage('name is required'),
+    body('email').notEmpty().withMessage('email is required').isEmail().withMessage('invalid email format').custom(async (email, { req }) => {
+        const user = await User.findOne({ email });
+        if (user && user._id.toString() !== req.user.userId) {
+            throw new Error('email already exists');
+        }
+    }),
+    body('lastName').notEmpty().withMessage('last name is required'),
+    body('location').notEmpty().withMessage('location is required'),
+]);
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { getApplicationStats, getCurrentUser, updateUser } from "../controllers/userController.js";
 import { updateJob } from "../controllers/jobController.js";
+import { validateUpdateUserInput } from "../middleware/validationMiddleware.js";
 const router = Router();
 
 
 router.get('/current-user', getCurrentUser);
 router.get('/admin/app-stats', getApplicationStats);
-router.patch('/update-user', updateUser);
+// check
+router.patch('/update-user', validateUpdateUserInput, updateUser);
 
 export default router;
