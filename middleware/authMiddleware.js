7466805f0526d5a8114b243e8cc2d396e858a464
YashRoushan <yashroushanprogramming@gmail.com>
2023-12-22 21:32:38 -0400

    get applications stats

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-import { UnauthenticatedError } from "../errors/customErrors.js";
+import { UnauthenticatedError, UnauthorizedError } from "../errors/customErrors.js";
 import { verifyJWT } from "../utils/tokenUtils.js";
 
 export const authenticateUser = (req, res, next) => {
@@ -14,3 +14,11 @@ export const authenticateUser = (req, res, next) => {
     }
 };
 
+export const authorizePermissions = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            throw new UnauthorizedError('Unauthorized to access this route')
+        }
+        next();
+    };
+};
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { getApplicationStats, getCurrentUser, updateUser } from "../controllers/userController.js";
 import { updateJob } from "../controllers/jobController.js";
 import { validateUpdateUserInput } from "../middleware/validationMiddleware.js";
+import { authorizePermissions } from "../middleware/authMiddleware.js";
 const router = Router();
 
 
 router.get('/current-user', getCurrentUser);
-router.get('/admin/app-stats', getApplicationStats);
+router.get('/admin/app-stats', [authorizePermissions('admin'), getApplicationStats]);
 // check
 router.patch('/update-user', validateUpdateUserInput, updateUser);
 
