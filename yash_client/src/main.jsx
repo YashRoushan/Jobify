d6fbc94a28cb659294dd8ade746405f7cdd79ba1
yash roushan <yashroushan@gmail.com>
2023-12-22 22:27:49 -0400

    axios

--- yash_client/src/main.jsx
+++ yash_client/src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import customFetch from './utils/customFetch.js'
 
 
-fetch('/api/v1/test').then((res) => res.json()).then((data) => console.log(data));
+
+// customFetch is calling via axios too.
+// const resp = await customFetch.get('/test');
+// console.log(resp);
+// fetch('/api/v1/test').then((res) => res.json()).then((data) => console.log(data));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
