4d6f2ff9db6117841e5213f1f69aa7fe20fd704b
yash roushan <yashroushan@gmail.com>
2023-12-28 16:22:39 -0400

    select input

--- yash_client/src/pages/AddJob.jsx
+++ yash_client/src/pages/AddJob.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FormRow } from '../components';
+import { FormRow, FormRowSelect } from '../components';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
 import { useOutletContext } from 'react-router-dom';
 import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants';
@@ -18,6 +18,20 @@ const AddJob = () => {
                 <FormRow type='text' name='position' />
                 <FormRow type='text' name='company' />
                 <FormRow type='text' labelText='job location' name='jobLocation' defaultValue={user.location} />
+
+                <FormRowSelect
+                    labelText='job status'
+                    name='jobStatus'
+                    defaultValue={JOB_STATUS.PENDING}
+                    list={Object.values(JOB_STATUS)}
+                />
+                <FormRowSelect
+                    labelText='job type'
+                    name='jobType'
+                    defaultValue={JOB_TYPE.FULL_TIME}
+                    list={Object.values(JOB_TYPE)}
+                />
+
                 <button type='submit' className='btn btn-block form-btn' disabled={isSubmitting}>
                     {isSubmitting ? 'submitting' : 'submit'}
                 </button>
