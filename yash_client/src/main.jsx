e3e6fde41bd2af55bdb39a2d640c6fc7f44225a8
YashRoushan <yashroushanprogramming@gmail.com>
2023-12-27 23:03:05 -0400

    react toastify

--- yash_client/src/main.jsx
+++ yash_client/src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import 'react-toastify/dist/ReactToastify.css'
 import './index.css'
-import customFetch from './utils/customFetch.js'
+import { ToastContainer } from 'react-toastify';
+// import customFetch from './utils/customFetch.js'
+
 
 
 
@@ -14,5 +17,6 @@ import customFetch from './utils/customFetch.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ToastContainer position='top-center'></ToastContainer>
   </React.StrictMode>,
 )
--- yash_client/src/pages/Register.jsx
+++ yash_client/src/pages/Register.jsx
@@ -3,16 +3,19 @@ import { Form, redirect, useNavigation, Link } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { FormRow, Logo } from '../components';
 import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
 export const action = async ({ request }) => {
     const formData = await request.formData();
     const data = Object.fromEntries(formData);
 
     try {
-        await customFetch.post('/auth/register', data)
+        await customFetch.post('/auth/register', data);
+        toast.success('Registeration successful');
         return redirect('/login');
     }
     catch (error) {
         console.log("this is an error -> " + error);
+        toast.error(error?.response?.data?.msg);
         return error;
     }
 }
