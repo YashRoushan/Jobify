55e1967134d1f48096210c452ff8b696d0330505
yash roushan <yashroushan@gmail.com>
2023-12-29 23:05:45 -0400

    All Jobs Structure

--- yash_client/src/App.jsx
+++ yash_client/src/App.jsx
@@ -5,6 +5,7 @@ import { action as registerAction } from './pages/Register';
 import { action as loginAction } from './pages/Login';
 import { action as addJobAction } from './pages/AddJob';
 import { loader as dashboardLoader } from './pages/DashboardLayout';
+import { loader as allJobsLoader } from './pages/AllJobs';
 
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
@@ -53,7 +54,8 @@ const router = createBrowserRouter([
           },
           {
             path: 'all-jobs',
-            element: <AllJobs />
+            element: <AllJobs />,
+            loader: allJobsLoader,
           },
           {
             path: 'profile',
