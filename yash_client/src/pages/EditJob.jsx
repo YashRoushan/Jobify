6e2403248964aae119576fc77653bfef94ee7266
yash roushan <yashroushan@gmail.com>
2023-12-31 17:05:58 -0400

    edit job action

--- yash_client/src/pages/EditJob.jsx
+++ yash_client/src/pages/EditJob.jsx
@@ -11,7 +11,6 @@ import customFetch from "../utils/customFetch";
 export const loader = async ({ params }) => {
     try {
         const { data } = await customFetch.get(`/jobs/${params.id}`);
-        toast.success('job edited!');
         return data;
     } catch (error) {
         toast.error(error?.response?.data?.msg || 'error!');
@@ -19,9 +18,17 @@ export const loader = async ({ params }) => {
     }
 }
 
-export const action = async () => {
-
-    return null;
+export const action = async ({ request, params }) => {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+    try {
+        await customFetch.patch(`/jobs/${params.id}`, data);
+        toast.success('Job Edited Successfully');
+        return redirect('/dashboard/all-jobs')
+    } catch (error) {
+        toast.error(error?.response?.data?.msg);
+        return error;
+    }
 }
 
 
@@ -31,31 +38,32 @@ const EditJob = () => {
     const isSubmitting = navigation.state === 'submitting';
     console.log(job);
     return <Wrapper>
-        <Form method='post' className='form' />
-        <h4 className='form-title'>
-            edit job
-        </h4>
-        <div className="form-center">
+        <Form method='post' className='form' >
+            <h4 className='form-title'>
+                edit job
+            </h4>
+            <div className="form-center">
 
-            <FormRow type='text' name="position" defaultValue={job.position} />
-            <FormRow type='text' name="company" defaultValue={job.company} />
-            <FormRow type='text' name="jobLocation" labelText='job location' defaultValue={job.jobLocation} />
-            <FormRowSelect
-                name='jobStatus'
-                labelText='job status'
-                defaultValue={job.jobStatus}
-                list={Object.values(JOB_STATUS)}
-            />
-            <FormRowSelect
-                name='jobType'
-                labelText='job type'
-                defaultValue={job.jobType}
-                list={Object.values(JOB_TYPE)}
-            />
-            <button type='submit' className='btn btn-block form-btn' disabled={isSubmitting}>
-                {isSubmitting ? 'submitting...' : 'submit'}
-            </button>
-        </div>
+                <FormRow type='text' name="position" defaultValue={job.position} />
+                <FormRow type='text' name="company" defaultValue={job.company} />
+                <FormRow type='text' name="jobLocation" labelText='job location' defaultValue={job.jobLocation} />
+                <FormRowSelect
+                    name='jobStatus'
+                    labelText='job status'
+                    defaultValue={job.jobStatus}
+                    list={Object.values(JOB_STATUS)}
+                />
+                <FormRowSelect
+                    name='jobType'
+                    labelText='job type'
+                    defaultValue={job.jobType}
+                    list={Object.values(JOB_TYPE)}
+                />
+                <button type='submit' className='btn btn-block form-btn' disabled={isSubmitting}>
+                    {isSubmitting ? 'submitting...' : 'submit'}
+                </button>
+            </div>
+        </Form >
     </Wrapper>
 }
 
