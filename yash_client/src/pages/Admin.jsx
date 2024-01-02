e69f54c8114c684e589fed8c318a8421500f3dab
yash roushan <yashroushan@gmail.com>
2024-01-02 15:40:19 -0400

    Admin page complete

--- yash_client/src/pages/Admin.jsx
+++ yash_client/src/pages/Admin.jsx
@@ -4,6 +4,7 @@ import { useLoaderData, redirect } from "react-router-dom";
 import customFetch from "../utils/customFetch";
 import Wrapper from "../assets/wrappers/StatsContainer";
 import { toast } from "react-toastify";
+import { StatItem } from '../components';
 
 
 export const loader = async () => {
@@ -19,7 +20,20 @@ const Admin = () => {
     const { users, jobs } = useLoaderData();
     return (
         <Wrapper>
-            <h1>Admin Page</h1>
+            <StatItem
+                title='current users'
+                count={users}
+                color="#e9b949"
+                bcg="#fcefc7"
+                icon={<FaSuitcaseRolling />}
+            />
+            <StatItem
+                title='total jobs'
+                count={jobs}
+                color="#647acb"
+                bcg="#e0e8f9"
+                icon={<FaCalendarCheck />}
+            />
         </Wrapper>
     )
 }
