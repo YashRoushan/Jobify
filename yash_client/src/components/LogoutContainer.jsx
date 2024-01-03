56222b30a6497c9d939616707ee6c35c7e622566
YashRoushan <yashroushanprogramming@gmail.com>
2024-01-02 23:04:45 -0400

    Updated User Functionality

--- yash_client/src/components/LogoutContainer.jsx
+++ yash_client/src/components/LogoutContainer.jsx
@@ -9,9 +9,16 @@ const LogoutContainer = () => {
 
     return (
         <Wrapper>
-            <button type='button' className='btn logout-btn'
-                onClick={() => { setShowLogout(!showLogout) }}>
-                <FaUserCircle />
+            <button
+                type='button'
+                className='btn logout-btn'
+                onClick={() => { setShowLogout(!showLogout) }}
+            >
+                {user.avatar ? (
+                    <img src={user.avatar} alt='avatar' className='img' />
+                ) : (
+                    <FaUserCircle />
+                )}
                 {user?.name}
                 <FaCaretDown />
             </button>
@@ -29,4 +36,3 @@ const LogoutContainer = () => {
 
 export default LogoutContainer;
 
-// todo tomorrow I will work on the theme toggle container, I can watch the previous video if I want for a better understanding of the project. Skip all the css videos as it can be done through chatgpt too. video 50 theme toggle
