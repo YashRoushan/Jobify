1b8d005b840e8d3ae65a2917d02e4dcdf9aeaf5f
YashRoushan <yashroushanprogramming@gmail.com>
2024-01-03 16:32:12 -0400

    Restrict access to test users

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { getApplicationStats, getCurrentUser, updateUser } from "../controllers/userController.js";
 import { updateJob } from "../controllers/jobController.js";
 import { validateUpdateUserInput } from "../middleware/validationMiddleware.js";
-import { authorizePermissions } from "../middleware/authMiddleware.js";
+import { authorizePermissions, checkForTestUser } from "../middleware/authMiddleware.js";
 import upload from "../middleware/multerMiddleware.js";
 const router = Router();
 
@@ -10,6 +10,6 @@ const router = Router();
 router.get('/current-user', getCurrentUser);
 router.get('/admin/app-stats', [authorizePermissions('admin'), getApplicationStats]);
 // check
-router.patch('/update-user', upload.single('avatar'), validateUpdateUserInput, updateUser);
+router.patch('/update-user', checkForTestUser, upload.single('avatar'), validateUpdateUserInput, updateUser);
 
 export default router;
