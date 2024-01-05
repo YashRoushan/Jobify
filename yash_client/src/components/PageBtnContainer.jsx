0a2f17095e02942cc06bf02831ab66e9c31432ac
yash roushan <yashroushan@gmail.com>
2024-01-05 12:18:03 -0400

    Render Button

--- yash_client/src/components/PageBtnContainer.jsx
+++ yash_client/src/components/PageBtnContainer.jsx
@@ -7,10 +7,28 @@ import { useAllJobsContext } from "../pages/AllJobs";
 const PageBtnContainer = () => {
 
     const { data: { numOfPages, currentPage } } = useAllJobsContext();
-    console.log(numOfPages);
-    return <h1>
-        pagebencontainer
-    </h1>
+    // underscored means that we are going to access the undefined
+    const pages = Array.from({ length: numOfPages }, (_, index) => {
+        return (index + 1)
+    });
+    console.log(pages);
+    return <Wrapper>
+        <button className='btn prev-btn' >
+            <HiChevronDoubleLeft />
+            prev
+        </button>
+        <div className="btn-container">
+            {pages.map((pageNumber) => {
+                return <button
+                    className={`btn page-btn ${pageNumber === currentPage && 'active'}`} key={pageNumber} > {pageNumber}
+                </button>
+            })}
+        </div>
+        <button className='btn next-btn' >
+            next
+            <HiChevronDoubleRight />
+        </button>
+    </Wrapper>
 
 }
 
