c3cae70f51329cb412b9e1787c9fa3b932a0357d
yash roushan <yashroushan@gmail.com>
2024-01-06 01:36:40 -0400

    Local Build

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-import 'express-async-errors';// ! this import should be always at the very very top of the page.
+import 'express-async-errors';
 import * as dotenv from 'dotenv';
 dotenv.config();
-// now we need to add the .js extension to import from a file on node unlike in vite.
 import express from "express";
 const app = express();
 import morgan from "morgan";
@@ -57,31 +56,17 @@ app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+})
 
-// this code was before the jobController folder was made
-// // but this was simple to understand
-// // ! GET ALL JOBS
-// app.get('/api/v1/jobs')
-// // ! CREATE JOB
-// app.post('/api/v1/jobs');
-// // ! GET SINGLE JOB
-// app.get('/api/v1/jobs/:id');
-// // ! EDIT JOB
-// app.patch('/api/v1/jobs/:id');
-// // ! DELETE JOBS
-// app.delete('/api/v1/jobs/:id');
 
-// this will work at the last
 app.use('*', (req, res) => {
     res.status(404).json({ msg: "not found" });
 });
 
 
-// this thing has been handled by errorHandlerMiddleware
-// app.use((err, req, res, next) => {
-//     console.log(err);
-//     res.status(500).json({ msg: "something went wrong in the server or the logic of your code." });
-// })
+
 app.use(errorHandlerMiddleware);
 
 
