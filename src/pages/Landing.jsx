33abe8d3d64401e302bee02c26795bcc407602ca
yash roushan <yashroushanprogramming@gmail.com>
2025-05-10 20:39:12 -0300

    Message updated

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -19,7 +19,7 @@ const Landing = () => {
                     <p>
                         Take control of your career with our job tracking app — organize applications, track progress,
                         and stay motivated every step of the way. Whether you're job hunting or leveling up, this
-                        platform empowers you to stay focused and land your dream role.
+                        platform empowers you to stay focused and land your dream role. You can explore the app instantly by clicking on <strong>Demo User</strong>!
                     </p>
                     <Link to='/register' className="btn register-link">
                         Register
